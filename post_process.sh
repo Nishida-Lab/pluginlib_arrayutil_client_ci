fd7ce363ae84a647568c439a2c2a79ded8e456ec
MoriKen254 <masamorobo@gmail.com>
2017-06-23 12:41:04 +0900

    Add source codes.

---
+++ post_process.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "This is post process!"
+echo "pwd"
+pwd
+echo "ls -al"
+ls -al
+echo "post process has been done!"
---
+++ pre_process.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "This is pre process!"
+echo "pwd"
+pwd
+echo "ls"
+ls
+echo "pre process has been done!"
